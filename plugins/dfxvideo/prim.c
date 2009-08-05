f17e942cc9e95674fbfe63c98d0af793b9aeb9b2
SND\weimingzhi_cp <SND\weimingzhi_cp@e17a0e51-4ae3-4d35-97c3-1a29b211df97>
2009-08-05 13:44:15 +0000

    git-svn-id: https://pcsxr.svn.codeplex.com/svn/pcsxr@24201 e17a0e51-4ae3-4d35-97c3-1a29b211df97

--- plugins/dfxvideo/prim.c
+++ plugins/dfxvideo/prim.c
@@ -592,9 +592,6 @@ void primBlkFill(unsigned char * baseAddr)
  sW+=sX;
  sH+=sY;
 
- if (sX < 0) sX=0;
- if (sY < 0) sY=0;
-
  FillSoftwareArea(sX, sY, sW, sH, BGR24to16(GETLE32(&gpuData[0])));
 
  bDoVSyncUpdate=TRUE;
