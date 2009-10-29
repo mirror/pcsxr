738bdfce50b464accf454e5130e952797e28ade7
SND\weimingzhi_cp <SND\weimingzhi_cp@e17a0e51-4ae3-4d35-97c3-1a29b211df97>
2009-10-29 10:28:02 +0000

    git-svn-id: https://pcsxr.svn.codeplex.com/svn/pcsxr@33751 e17a0e51-4ae3-4d35-97c3-1a29b211df97

--- plugins/dfxvideo/gpucfg-0.1df/main.c
+++ plugins/dfxvideo/gpucfg-0.1df/main.c
@@ -116,7 +116,7 @@ main (int argc, char *argv[])
 		const char *authors[]= {"Pete Bernert and the P.E.Op.S. team", "Ryan Schultz", "Andrew Burton", NULL};
 		widget = gtk_about_dialog_new ();
 		gtk_about_dialog_set_name (GTK_ABOUT_DIALOG (widget), "P.E.Op.S PCSX Video Plugin");
-		gtk_about_dialog_set_version (GTK_ABOUT_DIALOG (widget), "1.15");
+		gtk_about_dialog_set_version (GTK_ABOUT_DIALOG (widget), "1.17");
 		gtk_about_dialog_set_authors (GTK_ABOUT_DIALOG (widget), authors);
 		gtk_about_dialog_set_website (GTK_ABOUT_DIALOG (widget), "http://home.t-online.de/home/PeteBernert/");
 
