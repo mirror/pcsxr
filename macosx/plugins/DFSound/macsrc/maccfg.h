3c5463799445020fd0fe7ae8a4c9e604d8e5ad15
SND\edgbla_cp <SND\edgbla_cp@e17a0e51-4ae3-4d35-97c3-1a29b211df97>
2011-11-11 03:40:08 +0000

    Patch 10794.
    git-svn-id: https://pcsxr.svn.codeplex.com/svn/pcsxr@72167 e17a0e51-4ae3-4d35-97c3-1a29b211df97

---
+++ macosx/plugins/DFSound/macsrc/maccfg.h
@@ -0,0 +1,13 @@
+//
+//  maccfg.h
+//  PeopsSPU
+//
+
+#ifndef PeopsSPU_maccfg_h
+#define PeopsSPU_maccfg_h
+
+void DoAbout();
+long DoConfiguration();
+void LoadConfiguration();
+
+#endif
---
+++ macosx/plugins/PeopsXgl/macsrc/drawgl.h
@@ -0,0 +1,11 @@
+/* drawgl.h */
+
+unsigned long ulInitDisplay(void);
+void CloseDisplay(void);
+void BringContextForward(void);
+void DoBufferSwap(void);
+void SetVSync(long myValue);
+void BringContextForward(void);
+void ChangeWindowMode(void);
+void AboutDlgProc();
+void DlgProc();
