27e74d5981c2b963587c96c958b47b2f8ad16e48
SND\dario86_cp <SND\dario86_cp@e17a0e51-4ae3-4d35-97c3-1a29b211df97>
2011-11-26 02:16:26 +0000

    Added region autodetection for Net Yaroze Boot Disc (E) [DTL-S3035].
    git-svn-id: https://pcsxr.svn.codeplex.com/svn/pcsxr@72645 e17a0e51-4ae3-4d35-97c3-1a29b211df97

--- libpcsxcore/misc.c
+++ libpcsxcore/misc.c
@@ -332,8 +332,9 @@ int CheckCdrom() {
 
 	if (Config.PsxAuto) { // autodetect system (pal or ntsc)
 		if((CdromId[2] == 'e') || (CdromId[2] == 'E') ||
+			!strncmp(CdromId, "\0DTLS3035", 10) ||
 			!strncmp(CdromId, "PBPX95001", 10) || // according to redump.org, these PAL
-			!strncmp(CdromId, "PBPX95007", 10) || // demos have a non-standard ID;
+			!strncmp(CdromId, "PBPX95007", 10) || // discs have a non-standard ID;
 			!strncmp(CdromId, "PBPX95008", 10))   // add more serials if they are discovered.
 			Config.PsxType = PSX_TYPE_PAL; // pal
 		else Config.PsxType = PSX_TYPE_NTSC; // ntsc
