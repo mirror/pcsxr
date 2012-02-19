15f1bd53389bcad6da36439ee0a1a1383f51a085
SND\dario86_cp <SND\dario86_cp@e17a0e51-4ae3-4d35-97c3-1a29b211df97>
2012-02-19 19:17:01 +0000

    Updated Free Software Foundation address in two more files (Jem256).
    git-svn-id: https://pcsxr.svn.codeplex.com/svn/pcsxr@75200 e17a0e51-4ae3-4d35-97c3-1a29b211df97

--- plugins/dfxvideo/hq3x.h
+++ plugins/dfxvideo/hq3x.h
@@ -15,7 +15,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
  *
  * In addition, as a special exception, Andrea Mazzoleni
  * gives permission to link the code of this program with
