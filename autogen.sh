9bb4ff671b89d5f8d7605ce5e601c368b3cba673
SND\ckain_cp <SND\ckain_cp@e17a0e51-4ae3-4d35-97c3-1a29b211df97>
2014-02-16 09:09:53 +0000

    Merge branch 'missing_include_dir' of https://github.com/vilhelmgray/pcsxr
    git-svn-id: https://pcsxr.svn.codeplex.com/svn/pcsxr@89135 e17a0e51-4ae3-4d35-97c3-1a29b211df97

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,9 @@
 # Additional options go to configure.
 
 echo "Rebuilding ./configure with autoreconf..."
+if [ ! -d "include" ]; then
+  mkdir "include"
+fi
 autoreconf -f -i
 if [ $? -ne 0 ]; then
   echo "autoreconf failed"
