b2a6f1a70027737fd34df818be1310ca1d965234
SND\dario86_cp <SND\dario86_cp@e17a0e51-4ae3-4d35-97c3-1a29b211df97>
2014-02-23 16:15:26 +0000

    little fix for copyright notice
    git-svn-id: https://pcsxr.svn.codeplex.com/svn/pcsxr@89194 e17a0e51-4ae3-4d35-97c3-1a29b211df97

--- gui/AboutDlg.c
+++ gui/AboutDlg.c
@@ -71,7 +71,7 @@ void RunAboutDialog(void) {
 	const gchar *copyright = N_(
 		"(C) 1999-2003 PCSX Team\n"
 		"(C) 2005-2009 PCSX-df Team\n"
-		"(C) 2009-2013 PCSX-Reloaded Team");
+		"(C) 2009-2014 PCSX-Reloaded Team");
 
 	const gchar *license = N_(
 		"This program is free software: you can redistribute it and/or modify "
