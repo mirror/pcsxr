bc2deb45ebf01e79bbe3cc713b52f99851d14eed
SND\MaddTheSane_cp <SND\MaddTheSane_cp@e17a0e51-4ae3-4d35-97c3-1a29b211df97>
2014-04-12 21:55:37 +0000

    OS X: Comment additions/changes.
    git-svn-id: https://pcsxr.svn.codeplex.com/svn/pcsxr@89907 e17a0e51-4ae3-4d35-97c3-1a29b211df97

--- macosx/Pcsxr-QL/MyQuickLook.h
+++ macosx/Pcsxr-QL/MyQuickLook.h
@@ -14,6 +14,10 @@
 #include <CoreServices/CoreServices.h>
 #include <QuickLook/QuickLook.h>
 
+// Apple deprecated __private_extern__ in Xcode 4.6.
+// This is a convenience declaration to retain the old behavior.
+#define __private_extern__ __attribute__((visibility("hidden")))
+
 // The thumbnail generation function to be implemented in GenerateThumbnailForURL.c
 __private_extern__ OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize);
 __private_extern__ void CancelThumbnailGeneration(void* thisInterface, QLThumbnailRequestRef thumbnail);
