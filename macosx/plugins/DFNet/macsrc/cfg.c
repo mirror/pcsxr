264fac97c436e9df0c0e94daef463a1f0a17d3e8
SND\MaddTheSane_cp <SND\MaddTheSane_cp@e17a0e51-4ae3-4d35-97c3-1a29b211df97>
2014-11-10 19:24:58 +0000

    OS X: Remove an unused function in DFNet.
    git-svn-id: https://pcsxr.svn.codeplex.com/svn/pcsxr@92225 e17a0e51-4ae3-4d35-97c3-1a29b211df97

--- macosx/plugins/DFNet/macsrc/cfg.c
+++ macosx/plugins/DFNet/macsrc/cfg.c
@@ -11,8 +11,6 @@
 
 #include "dfnet.h"
 
-#define CFG_FILENAME "dfnet.cfg"
-
 void AboutDlgProc();
 void ConfDlgProc();
 void ReadConfig();
@@ -25,15 +23,6 @@ long NETconfigure() {
 	ConfDlgProc();
 	
 	return 0;
-}
-
-void SaveConf() {
-	FILE *f;
-
-	f = fopen(CFG_FILENAME, "w");
-	if (f == NULL) return;
-	fwrite(&conf, 1, sizeof(conf), f);
-	fclose(f);
 }
 
 void LoadConf() {
